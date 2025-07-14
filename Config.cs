e4584639b8eb322cee69c4cfe809008a2849dea0
Konoaru <jordan.jeanson31470@gmail.com>
2025-07-14 22:26:17 +0200

    added config.cs and plugin.cs

---
+++ Config.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Interfaces;
+
+namespace NoSpawn
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public int DelayHours { get; set; } = 0;
+        public int DelayMinutes { get; set; } = 40;
+        public int DelaySeconds { get; set; } = 0;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Exiled.Events.EventArgs.Server;
+using Respawning;
+using System;
+using ServerEvent = Exiled.Events.Handlers.Server;
+
+namespace NoSpawn
+{
+    public class NoSpawnPlugin : Plugin<Config>
+    {
+        public override string Name => "NoSpawn";
+        public override string Author => "Konoara";
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            if (Config.IsEnabled)
+                ServerEvent.RespawningTeam += OnRespawningTeam;
+        }
+
+        public override void OnDisabled()
+        {
+            ServerEvent.RespawningTeam -= OnRespawningTeam;
+        }
+
+        private void OnRespawningTeam(RespawningTeamEventArgs ev)
+        {
+            var delay = new TimeSpan(
+                Config.DelayHours,
+                Config.DelayMinutes,
+                Config.DelaySeconds
+            );
+
+            var faction = ev.Wave.SpawnableFaction;
+
+            if (Round.ElapsedTime < delay &&
+               (faction == SpawnableFaction.NtfWave ||
+                faction == SpawnableFaction.NtfMiniWave ||
+                faction == SpawnableFaction.ChaosWave ||
+                faction == SpawnableFaction.ChaosMiniWave))
+            {
+                ev.IsAllowed = false;
+                if (Config.Debug)
+                    Log.Info($"[NoSpawn] Bloqué {faction} à {Round.ElapsedTime}");
+            }
+        }
+    }
+}
